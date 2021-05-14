2d52b7c888ff1e69696371c68a6ea03cb15c50dd
Grigor Vardanyan <grigorvardanyan92@gmail.com>
2021-05-14 07:56:19 +0300

    minor changes

--- src/main/java/lt/codeacademy/App.java
+++ src/main/java/lt/codeacademy/App.java
@@ -1,6 +1,7 @@
 package lt.codeacademy;
 
 import lt.codeacademy.factory.NoteFactory;
+import lt.codeacademy.model.Note;
 import lt.codeacademy.service.NotebookHibernate;
 import lt.codeacademy.util.HibernateConfig;
 
@@ -8,30 +9,30 @@ public class App
 {
     public static void main( String[] args ) {
 
-        HibernateConfig.buildSessionFactory();
-
-        NotebookHibernate notebook = new NotebookHibernate();
-        NotebookInterface notebookInterface = new NotebookInterface(notebook);
-
-        // adding notes
-        notebook.saveNote(NoteFactory.getNote("for setting completed", "work"));
-        notebook.saveNote(NoteFactory.getNote("for changing text", "home"));
-        notebook.saveNote(NoteFactory.getNote("for deleting", "personal"));
-        notebook.saveNote(NoteFactory.getNote("just example", "personal"));
-
-        // mark note as completed
-        notebook.setNoteCompleted(1, true);
-
-        // change note text
-        notebook.updateNoteText(2, "changed text");
-
-        // delete note
-        notebook.deleteNote(3);
-
-        // print list
-        System.out.println(notebookInterface.uncompletedNotes());
-        System.out.println(notebookInterface.personalNotes());
-
-        HibernateConfig.closeSessionFactory();
+//        HibernateConfig.buildSessionFactory();
+//
+//        NotebookHibernate notebook = new NotebookHibernate();
+//        NotebookInterface notebookInterface = new NotebookInterface(notebook);
+//
+//        // adding notes
+//        notebook.saveNote(NoteFactory.getNote("for setting completed", "work"));
+//        notebook.saveNote(NoteFactory.getNote("for changing text", "home"));
+//        notebook.saveNote(NoteFactory.getNote("for deleting", "personal"));
+//        notebook.saveNote(NoteFactory.getNote("just example", "personal"));
+//
+//        // mark note as completed
+//        notebook.setNoteCompleted(1, true);
+//
+//        // change note text
+//        notebook.updateNoteText(2, "changed text");
+//
+//        // delete note
+//        notebook.deleteNote(3);
+//
+//        // print list
+//        System.out.println(notebookInterface.uncompletedNotes());
+//        System.out.println(notebookInterface.personalNotes());
+//
+//        HibernateConfig.closeSessionFactory();
     }
 }
